e46fd0ec7803a5123efb56b9d26d6779c039801a
System Administrator <root@MAC19S.local>
2019-02-07 00:09:37 +0530

    Commit 1 -- 2nd Computer

--- src/Programming/_13_HandlingFrames.java
+++ src/Programming/_13_HandlingFrames.java
@@ -1,42 +1,43 @@
 package Programming;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class _13_HandlingFrames {
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "/Users/love.vashista/Documents/Eclipse_Workspace/TEST/Selenium/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "/Users/love.vashista/Documents/Eclipse_Workspace/TEST/Selenium/geckodriver");
+		WebDriver driver = new FirefoxDriver();
 		
-		driver.get("https://jqueryui.com/resizable/");
+		driver.get("http://www.jqueryui.com/resizable");
 		
-		driver.switchTo().frame(0);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+		
+		WebElement resizePointer = driver.findElement(By.xpath("//div[@id='resizable']/child::div[3]"));
 		
 		Actions a = new Actions(driver);
 		
-		Thread.sleep(2000);
-		a.clickAndHold(driver.findElement(By.xpath("//div[@id='resizable']/div/following-sibling::div[2]"))).moveByOffset(300, 100).release().build().perform();
+		a.clickAndHold(resizePointer).moveByOffset(300, 100).release().build().perform();
 		
 		driver.switchTo().defaultContent();
 		
-		driver.findElement(By.linkText("Animate")).click();
+		driver.findElement(By.linkText("Droppable")).click();
 		
-		Thread.sleep(2000);
-		driver.get("http://jqueryui.com/droppable/");
-		
-		int TotalFrames = driver.findElements(By.tagName("iframe")).size();
-		System.out.println(TotalFrames);
+		int frameCountOnPage = driver.findElements(By.tagName("iframe")).size();
+		System.out.println("Total number of frames present on the page: " + frameCountOnPage);
 		
 		driver.switchTo().frame(0);
 		
 		WebElement source = driver.findElement(By.id("draggable"));
 		WebElement target = driver.findElement(By.id("droppable"));
-		a.dragAndDrop(source, target).build().perform();
 		
-		driver.switchTo().defaultContent();
+		a.dragAndDrop(source, target).build().perform();
 		
 		System.out.println("Success");
 	}
